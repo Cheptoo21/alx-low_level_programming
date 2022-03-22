ca205e68991c80725aba41eb229fbc6ded00fa6f
Cheptoo21 <cheptoopatience81@gmail.com>
2022-03-22 09:59:12 +0000

    10th task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,10 +11,10 @@ unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
 while (*(s + count) != '\0')
 {
-if (size > 0 $$ (*(s + count) < '0' || *(s + count) > '9'))
+if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 break;
 
-if (*(s + count) == '_')
+if (*(s + count) == '-')
 pn *= -1;
 
 if ((*(s + count) >= '0') && (*(s + count) <= '9'))

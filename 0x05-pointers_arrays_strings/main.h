b0393f6077abff2e782a95a40467cb15e22dd229
Cheptoo21 <cheptoopatience81@gmail.com>
2022-03-22 07:50:11 +0000

    Main file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,11 @@
-int -putchar(char c);
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-void _outs(char *str);
+void _puts(char *str);
+int _putchar(char);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);

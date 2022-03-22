33bc72c5d99b3c653e0ab940df8cacdb8cd7c67b
Cheptoo21 <cheptoopatience81@gmail.com>
2022-03-22 07:43:00 +0000

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
- *reset_to_98 - updates a integer value to 
+ *reset_to_98 - updates a integer value to
  *@n: pointer
+ *
+ *Return: void that means is correct
  */
 
  void reset_to_98(int *n)
